460318cf2ed3e554705766f69968c9dc42a795bf
lawrenceyeom <lawrenceyeom@outlook.com>
2025-05-18 20:48:37 +0800

    Initial commit: Prepare for Render deployment

--- frontend/src/constants.js
+++ frontend/src/constants.js
@@ -1,5 +1,5 @@
 // src/constants.js
-export const API_BASE = 'http://127.0.0.1:8000';
+export const API_BASE = 'https://four04-n-t-found-for-kpmg-2.onrender.com/';
 export const API_URL = API_BASE;
 
 export const COMPANIES = [
